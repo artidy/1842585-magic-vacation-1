a142fe6f4acac03a28b520b82b299a6273e1ae6e
Andrey Pechennikov <ap@devandy.me>
2022-06-06 18:41:50 +0300

    Добавил анимацию заголовка на главной странице

---
+++ source/js/functions.js
@@ -0,0 +1,57 @@
+const DEFAULT_SETTINGS = {
+  property: `transform`,
+  duration: 0.3,
+  timingFunctions: [`ease-in`, `ease-out`, `ease`],
+  lineSeparator: ` `,
+  containerTag: `span`,
+  lineClass: `line`,
+  letterClass: `letter`,
+  lineDelay: 0.2,
+  maxLetterDelay: 0.2,
+  letterDelayDigit: 1
+};
+
+export const modifyAnimateText = (textNode, settings = {}) => {
+  const animationSettings = {...DEFAULT_SETTINGS, ...settings};
+  const {
+    property,
+    duration,
+    timingFunctions,
+    lineSeparator,
+    containerTag,
+    lineClass,
+    letterClass,
+    lineDelay,
+    maxLetterDelay,
+    letterDelayDigit
+  } = animationSettings;
+  const content = textNode.textContent;
+  const lines = content.split(lineSeparator);
+
+  textNode.innerHTML = ``;
+
+  for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+    const lineElement = document.createElement(containerTag);
+    const line = lines[lineIndex];
+
+    lineElement.classList.add(lineClass);
+
+    for (const letter of line) {
+      const charElement = document.createElement(containerTag);
+      const timingFunction = timingFunctions[getRandomIndex(timingFunctions.length)];
+      const letterDelay = lineDelay * lineIndex + generateRandomNumber(maxLetterDelay, letterDelayDigit);
+
+      charElement.textContent = letter;
+      charElement.style.transition = `${property} ${duration}s ${timingFunction} ${letterDelay}s`;
+
+      charElement.classList.add(letterClass);
+      lineElement.append(charElement);
+    }
+
+    textNode.append(lineElement);
+  }
+};
+
+const getRandomIndex = (arrayLength) => generateRandomNumber(arrayLength - 1);
+
+const generateRandomNumber = (max, digit = 0) => +((Math.random() * max).toFixed(digit));
--- source/js/modules/general.js
+++ source/js/modules/general.js
@@ -1,5 +1,12 @@
+import {modifyAnimateText} from "../functions";
+
 export default () => {
+  const animateTitle = document.querySelector(`.intro__title`);
+
   window.addEventListener(`load`, function () {
     document.body.classList.add(`content_loaded`);
   });
+
+  animateTitle.classList.add(`animate-title`);
+  modifyAnimateText(animateTitle);
 };
