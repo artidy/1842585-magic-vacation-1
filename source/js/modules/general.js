2aea8a3e6b4fcb2a3e922c3c9fa57939b64723f8
Andrey Pechennikov <ap@devandy.me>
2022-06-06 19:23:58 +0300

    Добавил анимацию заголовков на остальных страницах

--- source/js/modules/general.js
+++ source/js/modules/general.js
@@ -3,6 +3,10 @@ import {modifyAnimateText} from "../functions";
 export default () => {
   const introTitle = document.querySelector(`.intro__title`);
   const introDate = document.querySelector(`.intro__date`);
+  const historyTitle = document.querySelector(`.slider__item-title`);
+  const prizeTitle = document.querySelector(`.prizes__title`);
+  const rulesTitle = document.querySelector(`.rules__title`);
+  const gameTitle = document.querySelector(`.game__title`);
 
   window.addEventListener(`load`, function () {
     document.body.classList.add(`content_loaded`);
@@ -10,4 +14,8 @@ export default () => {
 
   modifyAnimateText(introTitle);
   modifyAnimateText(introDate, {lineSeparator: `,`});
+  modifyAnimateText(historyTitle);
+  modifyAnimateText(prizeTitle);
+  modifyAnimateText(rulesTitle);
+  modifyAnimateText(gameTitle);
 };
